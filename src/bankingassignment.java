87a113c73e75803110bb3c42949070036e650a0d
Colton Weber <shrike76@aim.com>
2019-10-02 15:51:13 -0500

    Changed skeleton

---
+++ src/bankingassignment.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class bankingassignment {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to CIS-2348 Banking System!");
+        while(true){ System.out.println("Enter your Option in a number: 1. Display balance 2. Deposit amount 3. Withdraw amount");
+        int num = input.nextInt();
+           if (num==1){
+
+           }
+           else if (num==2){
+
+           }
+           else if (num==3){
+
+           }
+           else{
+               System.out.println("Error, Please input an appropriate value");
+               break;
+           }
+        }
+    }
+}
